6b9403c78107a2caba0d4d29766092e62097da3a
nwrs.LoPre@mail.ru <LoPre>
2017-01-25 01:37:28 +0300

    remove file db

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.34;dbname=testyii_testbase',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
