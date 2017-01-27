ffc69ce2972103fdf986c12573dbd81a0706fa2c
nwrs.LoPre@mail.ru <LoPre>
2017-01-27 11:17:22 +0300

    lesson12

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -168,7 +168,7 @@ class SiteController extends Controller
 
     public static function actionReleases()
     {
-        $query = Posts::find()->where(['hide' => 0, 'is_realese' => 1]);
+        $query = Posts::find()->where(['hide' => 0, 'is_release' => 1]);
 
         $pagination = new Pagination([
             'defaultPageSize' => 5,
